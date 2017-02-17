2663a9af94f94561529e7ae9e212b90d156ecb39
Omar Ahmad <eatmorerice@outlook.com>
2017-02-17 09:39:52 -0500

    Added platypus

---
+++ Platypus.java
@@ -0,0 +1,15 @@
+public class Platypus extends Pet
+{
+    public Platypus()
+    {
+        super("Perry");
+    }
+    public Platypus(String theName)
+    {
+        super(theName);
+    }
+    public String speak()
+    {
+        return "Nobody knows what sound these things make";
+    }
+}
